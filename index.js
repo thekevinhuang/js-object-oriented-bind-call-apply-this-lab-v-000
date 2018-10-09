bb8b11cada6ab4f5f6987a268256042dac127843
Kevin Huang <yellow.khaos@gmail.com>
2018-10-09 00:48:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 //Your code here
+function justInvoke (cb) {
+  let result = cb()
+  return result
+}
+
+function setThisWithCall(fn, thisValue, arg) {
+  let result = fn.call(thisValue, arg)
+  return result
+}
+
+function setThisWithApply(fn, thisValue, args) {
+  let result = fn.apply(thisValue, args)
+  return result
+}
+
+function returnNewFunctionOf(functionToBeCopied, thisValue){
+  let resultFunction = functionToBeCopied.bind(thisValue)
+  return resultFunction
+}
