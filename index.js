374ae70785898fae8a54e10f8be962caca1aa68c
Kevin Huang <yellow.khaos@gmail.com>
2018-10-09 00:37:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 //Your code here
+function justInvoke (cb) {
+  let result = cb()
+  return result
+}
+
+function setThisWithCall(fn, thisValue, arg) {
+  let result = fn.call(thisValue, arg)
+  return result
+}
+
+function setThisWithApply(fn, thisValue, args) {
+  let result = fn.apply(thisValue, args)
+  return result
+}
+
+function returnNewFucntionOf(functionToBeCopied, thisValue){
+  
+}
